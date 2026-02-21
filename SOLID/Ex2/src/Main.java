a381da23c3fce5a3d51d2ab1d12bf11d96da609f
Mehul Agarwal <agarwalmehul423@gmail.com>
2026-02-21 21:18:00 +0530

    assignment-2 completed

--- SOLID/Ex2/src/Main.java
+++ SOLID/Ex2/src/Main.java
@@ -4,16 +4,31 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("=== Cafeteria Billing ===");
 
-        CafeteriaSystem sys = new CafeteriaSystem();
+        TaxCalculator taxCalc = new TaxCalculator(List.of(
+                new StudentTaxRule(),
+                new StaffTaxRule(),
+                new DefaultTaxRule()));
+        DiscountCalculator distCalc = new DiscountCalculator(List.of(
+                new StudentDiscountRule(),
+                new StaffDiscountRule(),
+                new DefaultDiscountRule()));
+
+        CafeteriaSystem sys = new CafeteriaSystem(taxCalc, distCalc);
+
         sys.addToMenu(new MenuItem("M1", "Veg Thali", 80.00));
         sys.addToMenu(new MenuItem("C1", "Coffee", 30.00));
         sys.addToMenu(new MenuItem("S1", "Sandwich", 60.00));
 
         List<OrderLine> order = List.of(
                 new OrderLine("M1", 2),
-                new OrderLine("C1", 1)
-        );
+                new OrderLine("C1", 1));
 
         sys.checkout("student", order);
+
+        // Stretch Goal: staff member
+        List<OrderLine> staffOrder = List.of(
+                new OrderLine("S1", 2),
+                new OrderLine("C1", 2));
+        sys.checkout("staff", staffOrder);
     }
 }
