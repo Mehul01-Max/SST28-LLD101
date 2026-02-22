4b0f9a01b323843b2a3dc85bba635efc9e75f6d5
Mehul Agarwal <agarwalmehul423@gmail.com>
2026-02-22 18:52:59 +0530

    Assignment-1 completed

--- SOLID/Ex2/src/Main.java
+++ SOLID/Ex2/src/Main.java
@@ -1,34 +1,28 @@
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("=== Cafeteria Billing ===");
+        public static void main(String[] args) {
+                System.out.println("=== Cafeteria Billing ===");
 
-        TaxCalculator taxCalc = new TaxCalculator(List.of(
-                new StudentTaxRule(),
-                new StaffTaxRule(),
-                new DefaultTaxRule()));
-        DiscountCalculator distCalc = new DiscountCalculator(List.of(
-                new StudentDiscountRule(),
-                new StaffDiscountRule(),
-                new DefaultDiscountRule()));
+                TaxCalculator taxCalc = new TaxCalculator(List.of(
+                                new StudentTaxRule(),
+                                new StaffTaxRule(),
+                                new DefaultTaxRule()));
+                DiscountCalculator distCalc = new DiscountCalculator(List.of(
+                                new StudentDiscountRule(),
+                                new StaffDiscountRule(),
+                                new DefaultDiscountRule()));
 
-        CafeteriaSystem sys = new CafeteriaSystem(taxCalc, distCalc);
+                CafeteriaSystem sys = new CafeteriaSystem(taxCalc, distCalc);
 
-        sys.addToMenu(new MenuItem("M1", "Veg Thali", 80.00));
-        sys.addToMenu(new MenuItem("C1", "Coffee", 30.00));
-        sys.addToMenu(new MenuItem("S1", "Sandwich", 60.00));
+                sys.addToMenu(new MenuItem("M1", "Veg Thali", 80.00));
+                sys.addToMenu(new MenuItem("C1", "Coffee", 30.00));
+                sys.addToMenu(new MenuItem("S1", "Sandwich", 60.00));
 
-        List<OrderLine> order = List.of(
-                new OrderLine("M1", 2),
-                new OrderLine("C1", 1));
+                List<OrderLine> order = List.of(
+                                new OrderLine("M1", 2),
+                                new OrderLine("C1", 1));
 
-        sys.checkout("student", order);
-
-        // Stretch Goal: staff member
-        List<OrderLine> staffOrder = List.of(
-                new OrderLine("S1", 2),
-                new OrderLine("C1", 2));
-        sys.checkout("staff", staffOrder);
-    }
+                sys.checkout("student", order);
+        }
 }
